10911d16df76adefc6b6d3b7b171dc2aed583503
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-07-05 23:16:43 +0530

    Update AdminProductPage.js

--- src/Components/Pages/Admin/ProductPage.js/AdminProductPage.js
+++ src/Components/Pages/Admin/ProductPage.js/AdminProductPage.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AdminBase from '../../../../Base/ABase/AdminBase'
-import ProductCardLand from '../../Landing/ProductPageLand/card/ProdCardLand'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProducts } from '../../../../Base/redux/Products/productssAction'
+import ProductCardAdmin from './Card/prodCardAdmin'
 
 function AdminProductPage() {
+
+  const dispatch=useDispatch()
+  
+  useEffect(()=>{
+    dispatch(fetchProducts()) 
+   },[])
+  const product=useSelector((prod)=>prod.products.products)
   return (
     <AdminBase>
-        
+         
+      <div className="row justify-content-center products-main">
+        {product &&
+          product.map((prod) => (
+            <div
+              className="col-md-3 card-main"
+              key={prod._id}
+              
+            >
+              <ProductCardAdmin product={prod} />
+              
+            </div>
+           
+          ))}
+      </div>
+      
+      
     </AdminBase>
   )
 }
