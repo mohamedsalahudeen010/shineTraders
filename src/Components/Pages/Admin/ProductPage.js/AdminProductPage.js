91fdc89665b77b940b20b840d5b8ebed82c73377
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-07-06 11:26:44 +0530

    Update AdminProductPage.js

--- src/Components/Pages/Admin/ProductPage.js/AdminProductPage.js
+++ src/Components/Pages/Admin/ProductPage.js/AdminProductPage.js
@@ -2,7 +2,10 @@ import React, { useEffect } from 'react'
 import AdminBase from '../../../../Base/ABase/AdminBase'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchProducts } from '../../../../Base/redux/Products/productssAction'
-import ProductCardAdmin from './Card/prodCardAdmin'
+import ProductCardAdmin from './Card/ProductCardAdmin'
+import { fetchStock } from '../../../../Base/redux/Stock/stockAction'
+import { fetchCart } from '../../../../Base/redux/Cart/cartAction'
+
 
 
 function AdminProductPage() {
@@ -10,9 +13,17 @@ function AdminProductPage() {
   const dispatch=useDispatch()
   
   useEffect(()=>{
-    dispatch(fetchProducts()) 
-   },[])
-  const product=useSelector((prod)=>prod.products.products)
+    if(localStorage.getItem("email-admin")){
+      dispatch(fetchStock())
+    }
+    else if(localStorage.getItem("email")){
+      dispatch(fetchProducts()) 
+      dispatch(fetchCart(localStorage.getItem("email")))
+      console.log(localStorage.getItem("email"))
+    }
+  
+  },[])
+  const product=useSelector((stock)=>stock.stock.stock)
   return (
     <AdminBase>
          
