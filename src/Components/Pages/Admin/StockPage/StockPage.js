cd2e6f60650723ceef77e7f19ef4c7d20ccd34d8
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-07-06 11:27:07 +0530

    Update StockPage.js

--- src/Components/Pages/Admin/StockPage/StockPage.js
+++ src/Components/Pages/Admin/StockPage/StockPage.js
@@ -4,15 +4,25 @@ import "./StockPage.css"
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchProducts } from '../../../../Base/redux/Products/productssAction'
 import StockCardAdmin from './Card/StockCardAdmin'
+import { fetchStock } from '../../../../Base/redux/Stock/stockAction'
+import { fetchCart } from '../../../../Base/redux/Cart/cartAction'
 
 function StockPage() {
 
   const dispatch=useDispatch()
   
   useEffect(()=>{
-    dispatch(fetchProducts()) 
-   },[])
-  const product=useSelector((prod)=>prod.products.products)
+    if(localStorage.getItem("email-admin")){
+      dispatch(fetchStock())
+    }
+    else if(localStorage.getItem("email")){
+      dispatch(fetchProducts()) 
+      dispatch(fetchCart(localStorage.getItem("email")))
+      console.log(localStorage.getItem("email"))
+    }
+  
+  },[])
+  const product=useSelector((stock)=>stock.stock.stock)
   return (
    <AdminBase>
     <div className='stock-page'>
