a78d95907651d86fd013cdbc5858240aa89d6330
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-10-24 15:39:54 +0530

    Add files via upload

---
+++ src/Components/Pages/Main/AboutMain/AboutMain.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainBase from '../../../../Base/MBase/MainBase'
+import "./AboutMain.css"
+import About from '../../About/About'
+
+function AboutMain() {
+  return (
+    <MainBase>
+    <About></About>
+    </MainBase>
+  )
+}
+
+export default AboutMain
