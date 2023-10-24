5d7e56f1eb8c6772086d1ce6c8e40dc549fd3558
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-10-24 15:38:35 +0530

    Add files via upload

---
+++ src/Components/Pages/Landing/AboutLand/AboutLand.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./AboutLand.css"
+import LandingBase from '../../../../Base/LBase/LandingBase'
+import About from '../../About/About'
+
+
+function AboutLand() {
+  return (
+<LandingBase>
+    <About>
+        
+    </About>
+</LandingBase>
+    
+  )
+}
+
+export default AboutLand
