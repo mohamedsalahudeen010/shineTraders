b91329e464b2448d43518c028ef1c186825bf818
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-10-25 12:57:18 +0530

    Update OrdersPage.js

--- src/Components/Pages/Admin/OrdersPage/OrdersPage.js
+++ src/Components/Pages/Admin/OrdersPage/OrdersPage.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AdminBase from '../../../../Base/ABase/AdminBase'
 import "./OrdersPage.css"
 
 function OrdersPage() {
+
+  const [orders,setOrders]=useState()
+  const fetchOrders=async()=>{
+    
+     
+        try {
+            const response=await fetch(`https://shine-traders-back-end.vercel.app/order`,{
+                method:"GET",
+                headers:{
+                    "Content-Type":"application/json",
+                    "x-auth-token":localStorage.getItem("token")
+                 }
+            })
+            const data=await response.json()
+            console.log(data)
+          setOrders(data)
+          console.log(orders)
+        } 
+        catch (error) {
+            console.log(error)
+          
+        }
+    }
+
   return (
    <AdminBase>
 
