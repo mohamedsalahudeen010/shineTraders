434a31817502330efa85ec94d9e3361fcbf7c491
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-10-25 13:56:11 +0530

    Update OrdersPage.js

--- src/Components/Pages/Admin/OrdersPage/OrdersPage.js
+++ src/Components/Pages/Admin/OrdersPage/OrdersPage.js
@@ -1,34 +1,78 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminBase from '../../../../Base/ABase/AdminBase'
 import "./OrdersPage.css"
+import { useDispatch, useSelector } from 'react-redux'
+import { deleteOneItemOrder, fetchAllOrders } from '../../../../Base/redux/orders/ordersAction'
+import Plus from "@iconscout/react-unicons/icons/uil-plus-circle"
+import Minus from "@iconscout/react-unicons/icons/uil-minus-circle"
+import Trash from "@iconscout/react-unicons/icons/uil-trash-alt"
 
 function OrdersPage() {
 
-  const [orders,setOrders]=useState()
-  const fetchOrders=async()=>{
-    
-     
-        try {
-            const response=await fetch(`https://shine-traders-back-end.vercel.app/order`,{
-                method:"GET",
-                headers:{
-                    "Content-Type":"application/json",
-                    "x-auth-token":localStorage.getItem("token")
-                 }
-            })
-            const data=await response.json()
-            console.log(data)
-          setOrders(data)
-          console.log(orders)
-        } 
-        catch (error) {
-            console.log(error)
-          
-        }
-    }
+ const dispatch =useDispatch()
+
+    useEffect(()=>{
+      dispatch(fetchAllOrders())
+    },[])
+
+    const orders=useSelector((orders)=>orders.order.order);
+  
+    console.log(orders)
+  
+    const orderedProducts=orders.map((item,idx)=>item.products)
+    console.log(orderedProducts)
 
   return (
    <AdminBase>
+
+<div className="row justify-content-center admin-orders-page">
+      <div className="col-md-6 w-100">
+        <h1 className="my-cart">Orders</h1>
+
+        {orders.map((item,idx) => (
+          <div className="flex-container m-2 cart" key={idx}>
+            <div className="m-1 w-100" style={{ textAlign: "left" }}>
+              <h4>Order {idx+1}</h4>
+              <h5>
+             
+                Ordered By :
+                <b> {item.customerName}</b>
+              </h5>
+
+              <h4 style={{ display: "inline-block" }}>Email :  </h4>
+
+
+              <h6 style={{ display: "inline-block" }}>
+              
+                <b> {item.userMail}</b>
+                  
+               
+              </h6>
+              <hr></hr>
+            </div>
+
+            <div className="m-1 w-100 cart-image">
+              {/* <img src={item.productImage} alt=""></img> */}
+            </div>
+
+
+            <div className=" m-1 w-100 mt-5">
+              <h6>
+                {" "}
+               <Trash
+               color={"red"}
+               size={"2rem"}
+                onClick={() => {
+                  dispatch(deleteOneItemOrder(item));
+                }}
+                />
+              </h6>
+            </div>
+          </div>
+        ))}
+      </div>
+     
+    </div> 
 
    </AdminBase>
   )
